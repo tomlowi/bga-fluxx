a45e7414f280a20dec82fcfacf8e8781b4f9e61a
Iwan Tomlow <iwan.tomlow@gmail.com>
2021-05-02 17:23:27 +0200

    #40218: Inflation on Party Snacks goal

--- modules/php/Cards/Goals/GoalPartySnacks.php
+++ modules/php/Cards/Goals/GoalPartySnacks.php
@@ -16,7 +16,7 @@ class GoalPartySnacks extends GoalTwoKeepers
     $this->food_keepers = [3, 4, 5, 6];
   }
 
-  public function goalReachedByPlayer()
+  private function goalReachedByPlayerBasic()
   {
     $i = 0;
     $winner_id = null;
@@ -29,5 +29,52 @@ class GoalPartySnacks extends GoalTwoKeepers
     }
 
     return $winner_id;
+  }
+
+  private function goalReachedByPlayerWithInflation()
+  {
+    $cards = Utils::getGame()->cards;
+    $party_keeper_card = array_values(
+      $cards->getCardsOfType("keeper", $this->party_keeper)
+    )[0];
+    // Someone needs to have Party
+    if ($party_keeper_card["location"] != "keepers") {
+      return null;
+    }
+    $possibleWinner = $party_keeper_card["location_arg"];
+    // count how many food/snacks are also with this same player
+    $i = 0;
+    $food_count = 0;    
+    while ($i < count($this->food_keepers)) {
+      $food_keeper_card = array_values(
+        $cards->getCardsOfType("keeper", $this->food_keepers[$i])
+      )[0];
+
+      if ($food_keeper_card["location"] == "keepers"
+            && $food_keeper_card["location_arg"] == $possibleWinner) {
+          $food_count++;
+      }
+
+      $i++;
+    }
+
+    if ($food_count >= 2) {
+      return $possibleWinner;
+    }    
+
+    return null;
+  }
+
+  public function goalReachedByPlayer()
+  {
+    $activeInflation = Utils::getActiveInflation();
+
+    if ($activeInflation) {
+      // Inflation: need to have Party + any 2 snacks!
+      return $this->goalReachedByPlayerWithInflation();
+    } else {
+      // Basic: win with Party + 1 snack
+      return $this->goalReachedByPlayerBasic();
+    }
   }
 }
